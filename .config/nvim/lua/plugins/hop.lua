5ab0bba67187395d7089a6b5881d0f2571ca8e8b
ok6e <139907514+ok6e@users.noreply.github.com>
2023-08-05 21:25:43 +0300

    replace standard fF keymaps with hops

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -4,10 +4,10 @@ require('hop').setup {
 local hop = require('hop')
 local directions = require('hop.hint').HintDirection
 
-vim.keymap.set('', '<leader>f', function()
+vim.keymap.set('', 'f', function()
     hop.hint_char1({ direction = directions.AFTER_CURSOR })
 end, { remap = true })
 
-vim.keymap.set('', '<leader>F', function()
+vim.keymap.set('', 'F', function()
     hop.hint_char1({ direction = directions.BEFORE_CURSOR })
 end, { remap = true })
