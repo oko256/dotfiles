c605187b7902a621519a9f2c15528ca3be1c6639
oko256 <139907514+oko256@users.noreply.github.com>
2024-02-19 21:50:27 +0200

    Update nvim-treesitter.lua

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -7,4 +7,13 @@ require('nvim-treesitter.configs').setup {
         -- False here will disable the whole plugin
         enable = true,
     },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<CR>',
+            scope_incremental = '<CR>',
+            node_incremental = '<TAB>',
+            node_decremental = '<S-TAB>',
+        },
+    },
 }
